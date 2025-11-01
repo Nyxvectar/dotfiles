e600524dbdb11a9a6a916759abc571ec69069d83
Raye Lattice <nyxvectar@proton.me>
2025-11-01 20:23:35 +0800

    shell: tried to fix bugs

--- home/cae/cae.nix
+++ home/cae/cae.nix
@@ -8,5 +8,19 @@
 }:
 
 {
+#    programs.caelestia = {
+#        enable = true;
+#        cli.enable = true;
+#        systemd = {
+#            enable = true;
+#        };
+#        settings = {
+#            paths.wallpaperDir = "~/Pictures/Wallpapers";
+#            bar.status.showAudio = true;
+#            general.apps = {
+#                explorer = "thunar";
+#            };
+#        };
+#    };
     xdg.configFile."caelestia".source = ./caelestia;
 }
